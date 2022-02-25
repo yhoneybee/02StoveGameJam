1c15fbda6ec3b08b432df91d5bdb3049e678ca11
kkulbeol <chaeyh04@gmail.com>
2022-02-26 04:05:41 +0900

    카메라 시작

---
+++ Assets/Script/Cam.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    // Zoom용 카메라를 하나더 만들고 UI에다가 카메라가 보고있는거 Texture로 받아서 랜더링하기
+    [SerializeField] private float range = 4.5f;
+
+}
